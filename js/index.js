bc2ba8309f370b1405337a4b3df0864ae30e0727
Carlos <carlos_arellano@hotmail.ca>
2019-05-06 16:55:51 -0400

    created board and styling

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// player rolls dice 
+
+// player moves the amount of cells given by the dice 
+
+// board checks if the player falls on a normal cell, snake or ladder
+
+// if player falls on ladder, move the player upwards
+
+// if player falls on snake, move the player down
